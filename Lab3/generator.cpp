cc894b3e11e0183a981e4132f48e519e8a5a602a
ywang443 <ywang443@9d561d29-fe03-437e-93c4-c2db97216acc>
2015-02-09 00:07:30 +0000

    generator

--- Lab3/generator.cpp
+++ Lab3/generator.cpp
@@ -3,3 +3,22 @@
 
 // make generator
 // ./generator
+
+#include<stdio.h>
+
+int main() {
+int width =32;
+
+for(int i = 2; i < width; i++)
+printf(" alu1 a%d(out[%d], cout[%d], A[%d], B[%d], cout[%d], control);\n", i, i, i, i, i, i-1);
+  
+  printf("   wire  [%d:1] chain;\n\n", width - 1);
+
+  printf("   or o1(chain[1], out[0], out[1]);\n");
+  for (int i = 2 ; i < width ; i ++) {
+    printf("   or o%d(chain[%d], out[%d], chain[%d]);\n", i, i, i, i-1);
+  }
+  printf("   not n0(zero, chain[%d]);\n", width - 1);
+
+  return 0;
+}
