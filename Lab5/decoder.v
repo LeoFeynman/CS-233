b7eb13ad6e02f1242ca4b39273c8abda95195afc
ywang443 <ywang443@9d561d29-fe03-437e-93c4-c2db97216acc>
2015-02-19 23:41:28 +0000

    piazza-invalid module item

--- Lab5/decoder.v
+++ Lab5/decoder.v
@@ -13,5 +13,30 @@ module mips_decode(alu_op, writeenable, rd_src, alu_src2, except, opcode, funct)
     output [2:0] alu_op;
     output       writeenable, rd_src, alu_src2, except;
     input  [5:0] opcode, funct;
+    wire add, sub, and0, or0, nor0, xor0, addi, andi, ori, xori, 
+
+	assign add = `OP_OTHER0 & `OP0_ADD;
+	assign sub = `OP_OTHER0 & `OP0_SUB;
+	assign and0 = `OP_OTHER0 & `OP0_AND;
+	assign or0 = `OP_OTHER0 & `OP0_OR;
+	assign nor0 = `OP_OTHER0 & `OP0_NOR;
+	assign xor0 = `OP_OTHER0 & `OP0_XOR;
+
+	assign addi=`OP_ADDI;
+	assign andi=`OP_ANDI;
+	assign ori=`OP_ORI;
+	assign xori=`OP_XORI;
+
+	assign rd_src =  addi | andi | ori | xori;
+	assign alu_src2 =  addi | andi | ori | xori;
+
+	assign except = ~(add | sub | and0 | or0 | xor0 | nor0 | addi | andi | ori | xori);
+	assign writeenable = ~except;
+
+	assign alu_op[0] = (sub | or0 | xor0 | ori | xori);
+	assign alu_op[1] = (add | sub | xor0 | nor0 | addi | xori);
+	assign alu_op[2] = (and0 | or0 | xor0 | nor0 | andi | ori | xori);
+
+    
 
 endmodule // mips_decode
