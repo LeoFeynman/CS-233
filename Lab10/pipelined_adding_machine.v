cacd9646f0bf73aca8302def20e16cad38a2e400
ywang443 <ywang443@9d561d29-fe03-437e-93c4-c2db97216acc>
2015-03-31 16:31:14 +0000

    Lab10.1

--- Lab10/pipelined_adding_machine.v
+++ Lab10/pipelined_adding_machine.v
@@ -6,10 +6,17 @@ module pipelined_adding_machine(out, clk, reset);
 
    register #(30, 30'd0) Counter(index, next_index, clk, /* enable */1'b1, reset);
    adder30 Adder(next_index, index, 30'h1);
-      
+     
    adding_machine_memory rom (data, index);
-   alu32 alu(next_data, , `ALU_ADD, out, data);
+
+   register #(32, 32'd0) Pipelining(data_2, data, clk, /* enable */1'b1, reset);
+
+   alu32 alu(next_data, , `ALU_ADD, out, data_2);
 
    register #(32, 32'd0) Register(out, next_data, clk, /* enable */1'b1, reset);
-   
+   adder30 Adder2(next_index, index, 30'h1);
+
+
+
+ 
 endmodule // pipelined_machine
