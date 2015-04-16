3ffdfcd1d2ac5dd91a44ee132acabdd16c344af2
ywang443 <ywang443@9d561d29-fe03-437e-93c4-c2db97216acc>
2015-04-16 01:43:06 +0000

    Lab12.1

--- Lab12/simplecache.cpp
+++ Lab12/simplecache.cpp
@@ -2,9 +2,34 @@
 
 int SimpleCache::find(int index, int tag, int block_offset) {
   // read handout for implementation details
-  return 0xdeadbeef;
+	auto set = _cache.find(index);
+	for(auto block:set->second){
+		if(block.valid() == true && block.tag() == tag)
+		{
+		return block.get_byte(block_offset);
+		}
+	}
+	return 0xdeadbeef;
 }
 
 void SimpleCache::insert(int index, int tag, char data[]) {
   // read handout for implementation details
+	std::vector<SimpleCacheBlock> &cache = _cache[index];
+//	if(cache[index].valid() == true){
+//		cache[index].replace(tag, data);}
+	for(int i = 0; i < cache.size(); i++){
+		if(cache[i].valid() == false){
+//			block.replace(tag, data);
+			cache[i].replace(tag, data);
+			return;
+		}
+	}
+	
+	cache[0].replace(tag, data);
+//		auto set1 = _cache.find(0);
+//		for(auto block:set1->second){
+//		_cache.insert(0, set1.replace(tag, data));
+//		}
+	
+
 }
