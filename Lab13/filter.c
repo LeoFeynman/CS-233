c3e87a05f0893e98ff14ad1f5322f1904c9a538e
ywang443 <ywang443@9d561d29-fe03-437e-93c4-c2db97216acc>
2015-04-27 00:31:36 +0000

    Lab13.2

--- Lab13/filter.c
+++ Lab13/filter.c
@@ -7,14 +7,12 @@ void filter_fusion(pixel_t **image1, pixel_t **image2)
 {
 	for (int i = 1 ; i < SIZE-1 ; i++) {
 		filter1(image1, image2, i);
-	}
-	
-	for (int i = 2 ; i < SIZE-2 ; i ++) {
-		filter2(image1, image2, i);
-	}	
-		
-	for (int i = 1 ; i < SIZE-5 ; i ++) {
-		filter3(image2, i);
+		if(i >= 2 && i < SIZE-2){
+			filter2(image1, image2, i);
+		}
+		if(i < SIZE-5){
+			filter3(image2, i);
+		}
 	}		
 }
 
@@ -23,14 +21,19 @@ void filter_prefetch(pixel_t **image1, pixel_t **image2)
 {
 	for (int i = 1 ; i < SIZE-1 ; i++) {
 		filter1(image1, image2, i);
+		__builtin_prefetch (&image1[i+10], 0, 3);
+		__builtin_prefetch (&image2[i+11], 1, 3);
 	}
 	
 	for (int i = 2 ; i < SIZE-2 ; i ++) {
 		filter2(image1, image2, i);
+		__builtin_prefetch (&image1[i+10], 0, 3);
+		__builtin_prefetch (&image2[i+12], 1, 3);
 	}	
 		
 	for (int i = 1 ; i < SIZE-5 ; i ++) {
 		filter3(image2, i);
+		__builtin_prefetch (&image2[i+10], 0, 0);
 	}		
 }
 
@@ -40,13 +43,12 @@ void filter_all(pixel_t **image1, pixel_t **image2)
 {
 	for (int i = 1 ; i < SIZE-1 ; i++) {
 		filter1(image1, image2, i);
-	}
-	
-	for (int i = 2 ; i < SIZE-2 ; i ++) {
-		filter2(image1, image2, i);
-	}	
-		
-	for (int i = 1 ; i < SIZE-5 ; i ++) {
-		filter3(image2, i);
+		if(i < SIZE-3){
+			filter2(image1, image2, i + 1);
+		}	
+		if(i < SIZE-5){
+			filter3(image2, i);
+		}
+		__builtin_prefetch (&image2[i+10], 1, 0);
 	}		
 }
