3802ba3b66f3f7a64b0d629a5041a378c65bea45
ywang443 <ywang443@9d561d29-fe03-437e-93c4-c2db97216acc>
2015-05-03 00:31:20 +0000

    Lab14.2

--- Lab14/mandelbrot.cpp
+++ Lab14/mandelbrot.cpp
@@ -11,19 +11,26 @@
 int *
 mandelbrot_vector(float x[SIZE], float y[SIZE]) {
 	static int ret[SIZE];
-	float x1, y1, x2, y2;
-
+	float temp[4];
+	_m128 x1, x2, y1, y2, xx, yy, xy, X, Y, num2, x2sq, y2sq, acc;
 	for (int i = 0 ; i < SIZE ; i ++) {
-		x1 = y1 = 0.0;
-
+		x1 = y1 = _mm_set1_ps(0.0);
+		num2 = _mm_set1_ps(2.0);
 		// Run M_ITER iterations
 		for (int j = 0 ; j < M_ITER ; j ++) {
 			// Calculate the real part of (x1 + y1 * i)^2 + (x + y * i)
-			x2 = (x1 * x1) - (y1 * y1) + x[i];
-
+			//x2 = (x1 * x1) - (y1 * y1) + x[i];
+			X = _mm_loadu_ps(&x[i]);
+			xx = _mm_mul_ps(x1, x1);
+			yy = _mm_mul_ps(y1, y1);
+			x2 = _mm_sub_ps(xx, yy);
+			x2 = _mm_add_ps(x2, X);
 			// Calculate the imaginary part of (x1 + y1 * i)^2 + (x + y * i)
-			y2 = 2 * (x1 * y1) + y[i];
-
+			//y2 = 2 * (x1 * y1) + y[i];
+			Y = _mm_loadu_ps(&y[i]);
+			xy = _mm_mul_ps(x1, y1);
+			xy = _mm_mul_ps(num2, xy);
+			y2 = _mm_add_ps(xy, Y);
 			// Use the new complex number as input for the next iteration
 			x1 = x2;
 			y1 = y2;
@@ -32,7 +39,18 @@ mandelbrot_vector(float x[SIZE], float y[SIZE]) {
 		// caculate the magnitude of the result
 		// We could take the square root, but instead we just
 		// compare squares
-		ret[i] = ((x2 * x2) + (y2 * y2)) < (M_MAG * M_MAG);
+		//ret[i] = ((x2 * x2) + (y2 * y2)) < (M_MAG * M_MAG);
+		x2sq = _mm_mul_ps(x2, x2);
+		y2sq = _mm_mul_ps(y2, y2);
+		acc = _mm_add_ps(x2sq, y2sq);
+		m_magsq = _mm_mul_ps(M_MAG, M_MAG);
+		acc = _mm_cmplt_ps(acc, m_magsq);
+		
+		_mm_storeu_ps(temp, acc);
+		ret[i] = temp[0];
+		ret[i+1] = temp[1];
+		ret[i+2] = temp[2];
+		ret{i+3] = temp[3];
 	}
 
 	return ret;
--- Lab14/t6.c
+++ Lab14/t6.c
@@ -1,5 +1,6 @@
 #include "declarations.h"
-
+// A[i] D[i+1]
+// cannot be vectorized because D[i+1] depends on A[i]
 void 
 t6(float* A, float* D) {
 	for (int nl = 0 ; nl < ntimes ; nl ++) {
@@ -7,6 +8,7 @@ t6(float* A, float* D) {
 		for (int i = 0 ; i < (LEN6-1); i ++) {
 			A[i] = D[i] + (float)1.0;
 			D[i+1] = A[i] + (float)2.0;
+			
 		}
 	}      
 }
