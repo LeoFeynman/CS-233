5956b2dae78ec61bd4059c1787f80ec865d54b12
ywang443 <ywang443@9d561d29-fe03-437e-93c4-c2db97216acc>
2015-05-03 00:40:20 +0000

    Lab14.2

--- Lab14/t6.c
+++ Lab14/t6.c
@@ -5,6 +5,7 @@ void
 t6(float* A, float* D) {
 	for (int nl = 0 ; nl < ntimes ; nl ++) {
 		A[0] = 0;
+		#pragma novector
 		for (int i = 0 ; i < (LEN6-1); i ++) {
 			A[i] = D[i] + (float)1.0;
 			D[i+1] = A[i] + (float)2.0;
